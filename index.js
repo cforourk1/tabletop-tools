bc014285a34785ce41c7c4dea597aa69d8ad511c
Crystal <cforourk@umich.edu>
2026-03-24 16:49:15 -0400

    I went through and I added a lot of comments so I could explain it to myself when I inevetiably reference it again.

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
  * @returns {boolean} whether the wizard can cast the spell
  */
 function canCastSpell(isSpellPrepared, hasScroll) {
+  /*this function assesses if the wizard can cast the spell. It returns true if either argument passed into the parameters evaluates as true. the pipe operators side by side indicate the or operator.
+  */
     return isSpellPrepared || hasScroll
 }
 
@@ -17,7 +19,8 @@ function canCastSpell(isSpellPrepared, hasScroll) {
  * @returns {boolean} whether the creature is hidden from the observer
  */
 function isHidden(hiding, aware) {
-  return hiding || !aware
+  // this function assesses if the creature is hiding or not aware. I used pipe operators for the or portion and exclamation point to say is not aware
+    return hiding || !aware
 }
 
 /**
@@ -28,6 +31,7 @@ function isHidden(hiding, aware) {
  * @returns {boolean} whether the strike hits
  */
 function doesStrikeHit(attack, ac) {
+  // this function assesses if a strike hits. It assesses as true if the attack is greater than or equal to the targets armor class.
   return attack >= ac
 }
 
@@ -39,6 +43,7 @@ function doesStrikeHit(attack, ac) {
  * @returns {boolean} whether the strike is a critical hit
  */
 function doesStrikeCrit(attack, ac) {
+  // this function assesses if the strike it a critical hit. It only returns true if the attack is greater than armor class plus 10
   return attack > ac + 10
 }
 
@@ -51,6 +56,8 @@ function doesStrikeCrit(attack, ac) {
  * @returns {number} total hit points after healing
  */
 function heal(maxHp, currentHp, healAmount) {
+  /* I had to get help with this. math min is returning the smaller value there so I don't go over the max hp. I would like to see this talked about in class.
+  */
   return Math.min(currentHp + healAmount, maxHp);
 }
 
@@ -72,6 +79,8 @@ function heal(maxHp, currentHp, healAmount) {
  */
 function getProficiencyBonus(level, rank) {
   switch (rank) {
+    /*this function assesses what bonus a character gets based on rank. I used switch case here to made it simpler. Based on switch case of rank, I will return the bonus that corresponds with that rank. I had to add a default incase incorrect arguments were introduced.
+    */
     case "untrained":
      return level + 0
     break;
@@ -104,6 +113,8 @@ function getProficiencyBonus(level, rank) {
  * @returns {number} the cover bonus to AC
  */
 function getCoverBonus(behindObstacle, takingCover) {
+   /* here is where it got a bit more complicated. I needed to assess if the creature is behind an obstacle and if it is taking cover. So the firt if looks at is it behind an obstacle and taking cover - if true return 4. Then if it is true that it is behind an obstacle and NOT taking cover, return 2. So sneaky! But if it is not behind anything , or actively taking cover return 0 for 0 bonus.
+  */
   if (behindObstacle && takingCover) {
     return 4
   } else if (behindObstacle && !takingCover) {
@@ -126,6 +137,8 @@ function getCoverBonus(behindObstacle, takingCover) {
  * @returns {number} the creature's remaining HP after taking damage
  */
 function getRemainingHp(maxHp, currentHp, damage) {
+  /* for this function we had to assess if the damage was greater than or equal to double its max hp and then return a value of negative one. my first if uses an equation to assess if the damage is greater than or equal to maxhp times 2. the next line is if the damage is greater than or equal to current hp return 0 as the creature has depleted its hp. the last line assesses if the damage is less than the current hp, list the damage number dealt as the creatre is still alive.
+  */
    if (damage >= maxHp * 2) {
     return -1
    } else if (damage >= currentHp) {
@@ -144,6 +157,8 @@ function getRemainingHp(maxHp, currentHp, damage) {
  * @returns {boolean} whether the creature can see
  */
 function canSee(light, vision) {
+/* this function assesses what vision type a creature has and the conditions and returns values based on the argurments passed into the parameters. My first if statement looks at if the light is bright return true because all creatures can see in the bright light. My next one assesses if the light is dim and vision is low-light or dark return true because those two vision groups can see if the lighting is dim. I learned the hard way that parenthesis have to encapsulate the vision parameter AND vision has to be stated each time if I want to use the OR operator to return true. My next if returns true if it is dark out and the creature has a dark level of vision. Anything else is false.
+*/
     if (light === "bright") {
       return true
     } else if (light === "dim" && (vision === "low-light" || vision === "dark")) {
@@ -166,6 +181,8 @@ function canSee(light, vision) {
  * @returns {number} damage dealt by the strike
  */
 function getStrikeDamage(attack, ac, damage) {
+/* this one was particularly frustrating because you had to call the previous functions. So I called the function doesStrikeHit and its paramenters and assessed if it was critical in the same line. So - did the strike hit AND was it critical - if so then return damage * 2. The problem was again the () I didn't understant that I needed those for the if to work and it was similar to the issue in the light function where there is also an order of operations to consider. && happens BEFORE ||. My next if looks at if the strike hits, give me the damage report. Anything else is 0.
+*/
       if (doesStrikeHit(attack, ac) && doesStrikeCrit(attack, ac)) {
         return damage * 2
       } else if (doesStrikeHit(attack, ac)) {
